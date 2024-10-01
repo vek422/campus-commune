b063703ebfa2f7cded6b41e6a08c419422cdeedc
Vedant Kotkar <vedantkotkar111@gmail.com>
2024-10-01 19:47:30 +0530

    added commune model

--- backend/src/data/models/Commune.ts
+++ backend/src/data/models/Commune.ts
@@ -1,1 +1,37 @@
+import e from "express";
 import mongoose from "mongoose";
+
+interface ICommune extends mongoose.Document {
+    name: string;
+    description: string;
+    image_uri: string;
+    members: mongoose.Schema.Types.ObjectId[];
+    threads: mongoose.Schema.Types.ObjectId[];
+}
+
+const communeSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        // required: true,
+    },
+    image_uri: {
+        type: String,
+        // required: true,
+    },
+    members: {
+        type: [mongoose.Schema.Types.ObjectId],
+        ref: "User",
+    },
+    threads: {
+        type: [mongoose.Schema.Types.ObjectId],
+        ref: "Thread",
+    },
+});
+
+const CommuneModel = mongoose.model<ICommune>("Commune", communeSchema);
+
+export { CommuneModel };
